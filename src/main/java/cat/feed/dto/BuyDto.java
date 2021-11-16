f3742915a2f70ddda8948639420c9cfc871461af
hhhu04 <hhhu04@gmail.com>
2021-11-16 19:27:16 +0900

    거래요청, 승인 완료

--- src/main/java/cat/feed/dto/BuyDto.java
+++ src/main/java/cat/feed/dto/BuyDto.java
@@ -11,6 +11,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -23,26 +24,28 @@ public class BuyDto {
 
 
 
-    public void buy(String userId, BuyDto dto,String path) {
+    public List<String> buy(String userId, BuyDto dto, String path) {
         String admin = "23b3eeb10257d6952a5b9531776274de";
+        path ="http://"+  path + ":8080";
 
-        int tradeNumber = LocalDateTime.now().getYear()+LocalDateTime.now().getMonthValue()+
-                LocalDateTime.now().getHour()+LocalDateTime.now().getMinute()+LocalDateTime.now().getSecond();
+        String tradeNumber = Integer.toString(LocalDateTime.now().getYear())+Integer.toString(LocalDateTime.now().getMonthValue())+
+                Integer.toString(LocalDateTime.now().getHour())+Integer.toString(LocalDateTime.now().getMinute())+Integer.toString(LocalDateTime.now().getSecond());
 
         RestTemplate restTemplate = new RestTemplate();
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        String local =path + "/store/ok";
 
         params.add("cid","TC0ONETIME");
-        params.add("partner_order_id", Integer.toString(tradeNumber));
+        params.add("partner_order_id", tradeNumber);
         params.add("partner_user_id",userId);
         params.add("item_name","basket");
         params.add("quantity","1");
         params.add("total_amount",Integer.toString(dto.getTotalPrice()));
         params.add("tax_free_amount","0");
-        params.add("approval_url",path+"/store/ok");
-        params.add("cancel_url",path+"/store/cancel");
-        params.add("fail_url",path+"/store/fail");
+        params.add("approval_url",local);
+        params.add("cancel_url",path+"/store/ok");
+        params.add("fail_url",path+"/store/ok");
 
 
         String Authorization ="KakaoAK "+admin;
@@ -61,19 +64,67 @@ public class BuyDto {
                 String.class);
 
         ObjectMapper om = new ObjectMapper();
-        Map<String, Map<String,String>> map2=null;
         Map<String, String> map3=null;
-
+        List<String> list = new ArrayList<>();
 
         try {
-            map2 = om.readValue(response.getBody(), Map.class);
-            System.out.println(map2);
+            map3 = om.readValue(response.getBody(), Map.class);
+            list.add(map3.get("next_redirect_pc_url"));
+            list.add(tradeNumber);
+            list.add(map3.get("tid"));
+            return list;
+
         }
         catch (Exception e){
             e.printStackTrace();
+            list.add(e.getMessage());
+            return list;
         }
+    }
 
 
+    public Map<String, String> buySuccess(String userId, String tradeNumber, BuyDto dto, String tid, String pg_token) {
+        String admin = "23b3eeb10257d6952a5b9531776274de";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+
+        params.add("cid","TC0ONETIME");
+        params.add("tid",tid);
+        params.add("partner_order_id", tradeNumber);
+        params.add("partner_user_id",userId);
+        params.add("pg_token",pg_token);
+
+        String Authorization ="KakaoAK "+admin;
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Authorization",Authorization);
+
+
+        String url ="https://kapi.kakao.com/v1/payment/approve";
+
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params, httpHeaders);
+        ResponseEntity<String> response = restTemplate.exchange(
+                url, //{요청할 서버 주소}
+                HttpMethod.POST, //{요청할 방식}
+                entity, // {요청할 때 보낼 데이터}
+                String.class);
+
+        ObjectMapper om = new ObjectMapper();
+        Map<String, String> map3=null;
+        List<String> list = new ArrayList<>();
+
+        try {
+            map3 = om.readValue(response.getBody(), Map.class);
+            return map3;
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            map3.put("error",e.getMessage());
+            return map3;
+        }
 
 
     }
